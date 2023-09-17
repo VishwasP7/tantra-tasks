38a9b16e21b8b1a9444773ace4c4fe2f8aad52a4
VishwasP7 <122794474+VishwasP7@users.noreply.github.com>
2023-09-17 18:56:10 +0530

    Create SwapInt.java

---
+++ SwapInt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class swapInt {
+    public static void main(String[] args) {
+ int num1 = 45;
+ int num2 = 35;
+ System.out.println("First number is : " +num1);
+ System.out.println("Second number is : "+num2);
+ num1 = num1 + num2;
+ num2= num1-num2;
+ num1 = num1- num2;
+ System.out.println("First number is : " +num1);
+ System.out.println("Second number is : "+num2);
+    }
+}
